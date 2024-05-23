758ef91515085c051f79bb09861cddc2fbcb7765
MinhPhuc <phucnvm2210@gmail.com>
2024-05-23 20:13:56 +0700

    change type input object repo
    Signed-off-by: MinhPhuc <phucnvm2210@gmail.com>

--- goods-server.Infrastructure/Repositories/CategoryRepository.cs
+++ goods-server.Infrastructure/Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using goods_server.Core.InterfacesRepo;
+using goods_server.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace goods_server.Infrastructure.Repositories
 {
-    public class CategoryRepository : GenericRepository<CategoryRepository>,ICategoryRepo
+    public class CategoryRepository : GenericRepository<Category>,ICategoryRepo
     {
         public CategoryRepository(GoodsExchangeApplication2024DbContext dbContext) : base(dbContext)
         {
--- goods-server.Infrastructure/Repositories/CityRepository.cs
+++ goods-server.Infrastructure/Repositories/CityRepository.cs
@@ -1,4 +1,5 @@
 ﻿using goods_server.Core.InterfacesRepo;
+using goods_server.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace goods_server.Infrastructure.Repositories
 {
-    public class CityRepository : GenericRepository<CityRepository>, ICityRepo
+    public class CityRepository : GenericRepository<City>, ICityRepo
     {
         public CityRepository(GoodsExchangeApplication2024DbContext dbContext) : base(dbContext)
         {
